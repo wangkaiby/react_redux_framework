1cb4f5baee2f4ad16b0d26b6eec6eeafa4949d33
kathy <kathy@kathydeMBP.lan>
2019-08-28 22:37:27 +0800

    feat:ff

--- src/containers/Lazy.js
+++ src/containers/Lazy.js
@@ -1,12 +1,34 @@
-import React, {Suspense} from 'react'
-const OtherComponent = React.lazy(() => import('../components/Lazy'));
+// import React, {Suspense} from 'react'
+// const OtherComponent = React.lazy(() => import('../components/Lazy'));
 
-export default () => {
-    return(
-        <div>       
-            <Suspense fallback={<div>Loading...</div>}>
-                <OtherComponent/>
-            </Suspense>
-        </div>
-    )
+// export default () => {
+//     return(
+//         <div>       
+//             <Suspense fallback={<div>Loading...</div>}>
+//                 <OtherComponent/>
+//             </Suspense>
+//         </div>
+//     )
+// }
+import React, { Component } from 'react'
+
+export default class Lazy extends Component {
+    render() {
+        return (
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Name:
+                            <input
+                            defaultValue="Bob"
+                            type="text"
+                            ref={this.input} />
+                        </label>
+                        <input type="submit" value="Submit" />{this.props.name}
+                    </form>
+        )
+    }
 }
+Lazy.defaultProps = {
+    name: 'Mary'
+  };
+
